b9f74e9ae64195f2773c1e4458859f45ea0d6d13
Ghislain Demaël <demaelghislain@gmail.com>
2024-03-29 23:53:17 +0100

    Changed to work for both jpg and pngs, + it applies on all files of the directory

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
+#!python3
+
+import os
 from PIL import Image, ImageDraw
 
 
-def add_border_and_overlay(image_path, overlay_path, border_percent=1, overlay_percent=15):
+def addOverlay(image_path, overlay_path, border_percent=1, overlay_percent=15):
     img = Image.open(image_path)
-    size = img.size[0]
+    size = min(img.size)
 
     border_size = int(size * border_percent / 100)
 
     draw = ImageDraw.Draw(img)
 
-    x1 = 0
-    y1 = 0
-    x2 = size
-    y2 = size
-
-    draw.rectangle((x1, y1, x2, y1 + border_size), fill='red')
-    draw.rectangle((x1, y1, x1 + border_size, y2), fill='red')
-    draw.rectangle((x2 - border_size, y1, x2, y2), fill='red')
-    draw.rectangle((x1, y2 - border_size, x2, y2), fill='red')
+    draw.rectangle((0, 0, size, border_size), fill='red')
+    draw.rectangle((0, 0, border_size, size), fill='red')
+    draw.rectangle((size - border_size, 0, size, size), fill='red')
+    draw.rectangle((0, size - border_size, size, size), fill='red')
 
     overlay = Image.open(overlay_path)
     overlay_size = int(size * overlay_percent / 100)
     overlay = overlay.resize((overlay_size, overlay_size))
 
-    x = x2 - overlay_size
-    y = y2 - overlay_size
+    x = size - overlay_size
+    y = size - overlay_size
 
     img.paste(overlay, (x, y), overlay)
 
-    img.save('output.png')
+    img.save(os.path.splitext(image_path)[0] + '_ovd' + os.path.splitext(image_path)[1])
 
 
-add_border_and_overlay('blank1000.png', 'coin-signature.png', 1, 15)
+for filename in os.listdir('.'):
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        addOverlay(filename, 'assets/leCoin.png', 1, 15)
+
